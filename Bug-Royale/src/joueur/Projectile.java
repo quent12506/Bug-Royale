6381ba8d8281f39738961c3b108f0d3393e1dce8
Hugo <hugo.mas637@gmail.com>
2026-04-26 13:22:41 +0200

    debbuging projectile

--- Bug-Royale/src/joueur/Projectile.java
+++ Bug-Royale/src/joueur/Projectile.java
@@ -1,21 +1,25 @@
 package joueur;
 
+import outils.Coordonnee;
+
 /**
  * Classe représentant un projectile avec physique 2D (vitesse constante)
  * @author Xama66
  */
 public class Projectile {
     private Coordonnee position;      // Position du projectile
-    private Coordonnee direction;     // Direction du projectile (définie par l'utilisateur)
-    private Coordonnee vitesse;       // Vecteur vitesse
+    private Coordonnee cible;     // Direction du projectile (définie par l'utilisateur)
+    private Coordonnee direction;       // Vecteur direction normalise
+    private double vitesse;
     private double rayon;             // Rayon du projectile pour les collisions
     private boolean actif;            // Si le projectile est encore en vol
     private double temps;             // Temps écoulé depuis le lancement
 
     public Projectile(Coordonnee position, Coordonnee direction, double rayon) {
         this.position = position;
-        this.direction = direction;
-        this.vitesse = Coordonnee.vecteurDirection(this.position, this.direction);
+        this.cible = cible;
+        Coordonnee d = new Coordonnee();
+        this.direction = d.vecteurDirection(this.position, this.cible);
         this.rayon = rayon;
         this.actif = true;
         this.temps = 0;
@@ -29,7 +33,7 @@ public class Projectile {
         return direction;
     }
 
-    public Coordonnee getVitesse() {
+    public double getVitesse() {
         return vitesse;
     }
 
@@ -45,31 +49,29 @@ public class Projectile {
         return temps;
     }
 
-    public double getVitesseNorme() {
-        return vitesse.norm();
-    }
-
     public void setActif(boolean actif) {
         this.actif = actif;
     }
 
     public void setDirection(Coordonnee nouvelleDirection) {
-        this.direction = nouvelleDirection;
-        this.vitesse = Coordonnee.vecteurDirection(position, direction);
+        this.cible = nouvelleDirection;
+        Coordonnee d = new Coordonnee();
+        this.direction = d.vecteurDirection(position, cible);
     }
 
 
     public void lancerVersCible(Coordonnee cible) {
-        this.direction = cible;
-        this.vitesse = Coordonnee.vecteurDirection(position, direction);
+        this.cible = cible;
+        Coordonnee d = new Coordonnee();
+        this.direction = d.vecteurDirection(position, cible);
         this.actif = true;
     }
 
-    public void MAJ(double deltaT) {
+    public void MAJ(double deltaT) { //A MODIFIER
         if (!actif){
             return;
         }
-        Coordonnee deplacement = vitesse.mult(deltaT); // p = p0 + v*dt
+        Coordonnee deplacement = direction.mult(vitesse*deltaT); // p = p0 + v*dt
         position = position.add(deplacement);
 
         temps += deltaT;
@@ -77,7 +79,7 @@ public class Projectile {
 
 
     public boolean detecteCollisionCercle(Coordonnee centre, double rayon) {
-        Coordonnee positionSuivante = position.add(vitesse.mult(0.016)); // ~16ms pour 60 FPS
+        Coordonnee positionSuivante = position.add(direction.mult(0.016*vitesse)); // ~16ms pour 60 FPS
         return Coordonnee.segmentIntercepteCercle(position, positionSuivante, centre, rayon + this.rayon);
     }
 
@@ -86,17 +88,16 @@ public class Projectile {
     }
 
 
-    public Coordonnee calculerPositionAuTemps(double t) {
-        Coordonnee pos0 = position.sub(vitesse.mult(temps));
-        return pos0.add(vitesse.mult(t));
+    public Coordonnee calculerPositionAuTemps(double t) {//A MODIFIER
+        Coordonnee pos0 = position.sub(direction.mult(temps*vitesse));
+        return pos0.add(direction.mult(t*vitesse));
     }
 
 
     public Coordonnee calculerPositionADistance(double distance) {
         if (distance < 0) return null;
-        double vitesseNorme = vitesse.norm();
-        if (vitesseNorme == 0) return position;
-        double t = distance / vitesseNorme;
+        if (vitesse == 0) return position;
+        double t = distance / vitesse;
         return calculerPositionAuTemps(temps + t);
     }
 
@@ -104,15 +105,16 @@ public class Projectile {
  
     public void arreter() {
         this.actif = false;
-        this.vitesse = new Coordonnee(0, 0);
+        this.vitesse = 0;
     }
 
     @Override
     public String toString() {
         return String.format("Projectile{pos=(%f,%f), dir=(%f,%f), vitesse=(%f,%f), rayon=%f, actif=%b}",
                 position.getx(), position.gety(),
+                cible.getx(), cible.gety(),
                 direction.getx(), direction.gety(),
-                vitesse.getx(), vitesse.gety(),
+                
                 rayon, actif);
     }
 }
