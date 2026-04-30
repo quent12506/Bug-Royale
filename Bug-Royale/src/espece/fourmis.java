3f649d235960d688a45f40c93572534c2c77d6bc
alexpagesmarseille-jpg <alexpagesmarseille@gmail.com>
2026-04-30 08:43:33 +0200

    Create fourmis.java

---
+++ Bug-Royale/src/espece/fourmis.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espece;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import joueur.Joueur;
+
+/**
+ *
+ * @author hugom
+ */
+public class fourmis extends Espece{
+
+    public fourmis() {
+        super("fourmis", 7.5, 75, null);
+        try {
+            this.sprite = ImageIO.read(getClass().getResource("../resources/fourmis.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(Joueur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+}
